9586b2e4a22231cd6c1490ccb0cd00425dd5640b
abidex4yemi <abidex4yemi@gmail.com>
2020-01-19 17:02:44 +0100

    type annotation vs type inference

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -40,3 +40,39 @@ const point: { x: number; y: number } = {
 
 // This will give type error
 // point.x = 'yemi';
+
+// Function
+
+const logNumber: (value: number) => void = (value: number) => {
+  console.log(value);
+};
+
+// When to use annotations
+// 1) Function that returns the "any" type
+
+const json = '{"x": 20, "y": 30}';
+const coordinate: { x: number; y: number } = JSON.parse(json);
+console.log(coordinate); // {x: 20, y: 30}
+
+//2) When we declare variable on one line and assigned value to it later
+let words = ['Red', 'Green', 'Blue'];
+let foundWord = false;
+
+words.forEach(word => {
+  if (word === 'Blue') {
+    foundWord = true;
+  }
+});
+
+console.log(foundWord);
+
+//3) When inference doesn't work
+const numbers = [-10, -20, -5];
+
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
