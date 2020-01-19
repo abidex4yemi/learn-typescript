4cf88ee2a70b893a74d9428b4b1fcc8e7e306aa4
abidex4yemi <abidex4yemi@gmail.com>
2020-01-19 17:50:47 +0100

    using typescripts with object

---
+++ features/annotations/objects.ts
@@ -0,0 +1,21 @@
+const profile = {
+  name: 'Alex',
+  age: 20,
+  coords: {
+    lat: 0,
+    lng: 15
+  },
+  setAge(age: number): void {
+    this.age = age;
+  }
+};
+
+const { age }: { age: number } = profile;
+console.log(age);
+
+// more complex destructuring
+const {
+  coords: { lat, lng }
+}: { coords: { lat: number; lng: number } } = profile;
+
+console.log(lat, lng);
