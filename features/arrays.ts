9ac39d0bdabd79d5d03170b5e7b7ec01fcf04101
abidex4yemi <abidex4yemi@gmail.com>
2020-01-25 22:15:28 +0100

    Using typed array

---
+++ features/arrays.ts
@@ -0,0 +1,20 @@
+const carMakers = ['Toyota', 'Chevy', 'Ford'];
+const dates = [new Date(), new Date()];
+
+const carsByMake = [['f150'], ['corolla'], ['camaro']];
+
+// Typed array helps with typed inference
+const newCar = carMakers[0];
+const myCar = carMakers.pop();
+
+// prevent incompatible value
+//carMakers.push(300)
+
+// helps with autocompletion when using map() method
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
+
+// Flexible types
+const importantDates: (string | Date)[] = [new Date()];
+importantDates.push('2010-12-9');
