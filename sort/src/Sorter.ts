e8e25f28eb1b752b721c9611fe6bbe2223210bbe
abidex4yemi <abidex4yemi@gmail.com>
2020-03-06 12:30:50 +0100

    refactor sorter class to absttract class

--- sort/src/Sorter.ts
+++ sort/src/Sorter.ts
@@ -1,15 +1,15 @@
-import { Sortable } from './interfaces';
+export abstract class Sorter {
+  abstract length: number;
 
-export class Sorter {
-  constructor(public collection: Sortable) {}
+  abstract compare(leftIndex: number, rightIndex: number): Boolean;
+
+  abstract swap(leftIndex: number, rightIndex: number): void;
 
   sort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection.compare(j, j + 1)) {
-          this.collection.swap(j, j + 1);
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length - i - 1; j++) {
+        if (this.compare(j, j + 1)) {
+          this.swap(j, j + 1);
         }
       }
     }
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -3,23 +3,22 @@ import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
-// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+numbersCollection.sort();
 
-// console.log(numbersCollection.data);
+console.log(numbersCollection.data);
 
 // const charactersCollection = new CharactersCollection('Goody');
 // const sorter = new Sorter(charactersCollection);
 // sorter.sort();
 
-const linkedList = new LinkedList();
-linkedList.addToTail(10);
-linkedList.addToTail(1);
-linkedList.addToTail(2);
-linkedList.addToTail(0);
+// const linkedList = new LinkedList();
+// linkedList.addToTail(10);
+// linkedList.addToTail(1);
+// linkedList.addToTail(2);
+// linkedList.addToTail(0);
 
-const sorter = new Sorter(linkedList);
-sorter.sort();
+// const sorter = new Sorter(linkedList);
+// sorter.sort();
 
-console.log(linkedList.print());
+// console.log(linkedList.print());
