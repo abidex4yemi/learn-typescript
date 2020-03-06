8c0687236eb312cd39d8232a024fc7a87a2bcc16
abidex4yemi <abidex4yemi@gmail.com>
2020-03-06 11:54:57 +0100

    Add: linked list sorting capability

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 // const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 // const sorter = new Sorter(numbersCollection);
@@ -8,8 +9,17 @@ import { CharactersCollection } from './CharactersCollection';
 
 // console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection('Goody');
-const sorter = new Sorter(charactersCollection);
+// const charactersCollection = new CharactersCollection('Goody');
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+
+const linkedList = new LinkedList();
+linkedList.addToTail(10);
+linkedList.addToTail(1);
+linkedList.addToTail(2);
+linkedList.addToTail(0);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
 
-console.log(charactersCollection.data);
+console.log(linkedList.print());
