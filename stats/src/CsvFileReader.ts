05ce73cba2eef2da364fa5362a3f90679bfda361
abidex4yemi <abidex4yemi@gmail.com>
2020-03-31 15:40:35 +0100

    Add: csv reader class

---
+++ stats/src/CsvFileReader.ts
@@ -0,0 +1,13 @@
+import fs from 'fs';
+
+export class CsvFileReader {
+  data: String[][] = [];
+  constructor(public filename: String) {}
+
+  read(): void {
+    this.data = fs
+      .readFileSync(`${this.filename}`, { encoding: 'utf-8' })
+      .split('\n')
+      .map((row: string): string[] => row.split(','));
+  }
+}
